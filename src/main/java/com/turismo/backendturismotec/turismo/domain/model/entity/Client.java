adffbb819bc5bb7be5b12005018fffe453f2b180
RodrigoHuarcayaGalarza <89543373+RodrigoHuarcayaGalarza@users.noreply.github.com>
2022-06-14 17:29:13 -0500

    Add: Client Entity Implementation

--- src/main/java/com/turismo/backendturismotec/turismo/domain/model/entity/Client.java
+++ src/main/java/com/turismo/backendturismotec/turismo/domain/model/entity/Client.java
@@ -1,4 +1,49 @@
 package com.turismo.backendturismotec.turismo.domain.model.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Entity
+@Table(name = "clients")
 public class Client {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    @NotBlank
+    private String email;
+
+    @NotNull
+    @NotBlank
+    private String address;
+
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @NotNull
+    @NotBlank
+    private String password;
+  
 }

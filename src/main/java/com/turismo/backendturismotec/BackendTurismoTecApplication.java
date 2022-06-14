fe30e7870f8ce4bc1302995dbd98854ead9884cc
David Reategui <reateguicevallos.darh@gmail.com>
2022-06-14 16:24:20 -0500

    Add: Initial commit

---
+++ src/main/java/com/turismo/backendturismotec/BackendTurismoTecApplication.java
@@ -0,0 +1,13 @@
+package com.turismo.backendturismotec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTurismoTecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTurismoTecApplication.class, args);
+    }
+
+}

be43acb9fea17d47b1a26f4c3b6dab494889c337
RodrigoHuarcayaGalarza <89543373+RodrigoHuarcayaGalarza@users.noreply.github.com>
2022-06-14 18:17:59 -0500

    Add: Mapping Configuration

--- src/main/java/com/turismo/backendturismotec/turismo/mapping/MappingConfiguration.java
+++ src/main/java/com/turismo/backendturismotec/turismo/mapping/MappingConfiguration.java
@@ -1,4 +1,18 @@
-package com.turismo.backendturismotec.turismo.mapping;
+package com.turismo.turismoapi.turismo.mapping;
 
+import com.turismo.turismoapi.turismo.domain.model.entity.Client;
+import com.turismo.turismoapi.turismo.domain.model.entity.Companies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("turismoMappingConfiguration")
 public class MappingConfiguration {
+    @Bean
+    public ClientMapper clientMapper() {
+        return new ClientMapper();
+    }
+    @Bean
+    public CompanyMapper companyMapper() { return new CompanyMapper(); }
+    @Bean
+    public PackageMapper packageMapperMapper() { return new PackageMapper(); }
 }

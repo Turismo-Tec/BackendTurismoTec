916a7da47a3cc35abc29004a9d02fda064c169af
Jef de la cruz <105805284+jimmy110498@users.noreply.github.com>
2022-06-14 18:26:56 -0500

    Add: Client Service Implmentation

--- src/main/java/com/turismo/backendturismotec/turismo/domain/service/ClientService.java
+++ src/main/java/com/turismo/backendturismotec/turismo/domain/service/ClientService.java
@@ -1,4 +1,14 @@
-package com.turismo.backendturismotec.turismo.domain.service;
+package com.turismo.turismoapi.turismo.domain.service;
+
+import com.turismo.turismoapi.turismo.domain.model.entity.Client;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface ClientService {
+    List<Client> getAll();
+    Client getById(Long clientId);
+    Client create(Client client);
+    Client update(Long id, Client request);
+    ResponseEntity<?> delete(Long clientId);
 }

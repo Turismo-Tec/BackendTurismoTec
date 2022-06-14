ae85b6b06443755049fcf366ffe23dc005d0d2d3
RodrigoHuarcayaGalarza <89543373+RodrigoHuarcayaGalarza@users.noreply.github.com>
2022-06-14 17:17:06 -0500

    Add: Client Controller Implmentation

--- src/main/java/com/turismo/backendturismotec/turismo/api/ClientController.java
+++ src/main/java/com/turismo/backendturismotec/turismo/api/ClientController.java
@@ -1,4 +1,53 @@
 package com.turismo.backendturismotec.turismo.api;
 
+import com.turismo.turismoapi.turismo.domain.service.ClientService;
+import com.turismo.turismoapi.turismo.mapping.ClientMapper;
+import com.turismo.turismoapi.turismo.resources.client.ClientResource;
+import com.turismo.turismoapi.turismo.resources.client.CreateClientResource;
+import com.turismo.turismoapi.turismo.resources.client.UpdateClientResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/clients")
 public class ClientController {
+  
+   private final ClientService clientService;
+    private final ClientMapper mapper;
+
+    public ClientController(ClientService clientService, ClientMapper mapper){
+        this.clientService = clientService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public Page<ClientResource> getAllClients(Pageable pageable) {
+        return mapper.modelListPage(clientService.getAll(), pageable);
+    }
+
+    @GetMapping("{clientId}")
+    public ClientResource getClientsById(@PathVariable Long clientId){
+        return mapper.toResource(clientService.getById(clientId));
+    }
+
+    @PostMapping
+    public ClientResource createClients(@RequestBody CreateClientResource resource){
+        return mapper.toResource(clientService.create(mapper.toModel(resource)));
+    }
+
+    @PutMapping("{clientId}")
+    public ClientResource updateClients(@PathVariable Long clientId,
+                                           @RequestBody UpdateClientResource resource){
+        return mapper.toResource(clientService.update(clientId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{clientId}")
+    public ResponseEntity<?> deleteClients(@PathVariable Long clientId){
+        return clientService.delete(clientId);
+    }
+  
 }

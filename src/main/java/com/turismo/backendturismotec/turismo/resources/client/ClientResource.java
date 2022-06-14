963509cdd5f98d2e885f329ef7c92289688c438f
ChrisitanZ <102604546+ChrisitanZ@users.noreply.github.com>
2022-06-14 17:40:15 -0500

    Add: Client Resource Implementation

--- src/main/java/com/turismo/backendturismotec/turismo/resources/client/ClientResource.java
+++ src/main/java/com/turismo/backendturismotec/turismo/resources/client/ClientResource.java
@@ -1,4 +1,19 @@
-package com.turismo.backendturismotec.turismo.resources.client;
+package com.turismo.turismoapi.turismo.resources.client;
 
+import lombok.*;
+
+@Setter
+@Getter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientResource {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int phone;
+    private String email;
+    private String address;
+    private String username;
+    private String password;
 }

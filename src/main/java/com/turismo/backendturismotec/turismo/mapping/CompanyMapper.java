13cd8a29e78c8d5f5de41a9b49d0062007dc89ee
RodrigoHuarcayaGalarza <89543373+RodrigoHuarcayaGalarza@users.noreply.github.com>
2022-06-14 18:16:40 -0500

    Add: Company Mapper Implementation

--- src/main/java/com/turismo/backendturismotec/turismo/mapping/CompanyMapper.java
+++ src/main/java/com/turismo/backendturismotec/turismo/mapping/CompanyMapper.java
@@ -1,4 +1,35 @@
-package com.turismo.backendturismotec.turismo.mapping;
+package com.turismo.turismoapi.turismo.mapping;
+
+import com.turismo.turismoapi.shared.mapping.EnhanceModelMapper;
+import com.turismo.turismoapi.turismo.domain.model.entity.Companies;
+import com.turismo.turismoapi.turismo.resources.company.CompanyResource;
+import com.turismo.turismoapi.turismo.resources.company.CreateCompanyResource;
+import com.turismo.turismoapi.turismo.resources.company.UpdateCompanyResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public class CompanyMapper {
+    @Autowired
+    EnhanceModelMapper mapper;
+
+    public CompanyResource toResource(Companies model) {
+        return mapper.map(model, CompanyResource.class);
+    }
+
+    public Page<CompanyResource> modelListPage(List<Companies> modelList, Pageable pageable) {
+        return new PageImpl<>(mapper.mapList(modelList, CompanyResource.class),
+                pageable, modelList.size());
+    }
+
+    public Companies toModel(CreateCompanyResource resource) {
+        return mapper.map(resource, Companies.class);
+    }
+
+    public Companies toModel(UpdateCompanyResource resource) {
+        return mapper.map(resource, Companies.class);
+    }
 }

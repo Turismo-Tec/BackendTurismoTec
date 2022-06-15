933199090e1d3fda80c32e3a65d9aff524bd94ed
Jef de la cruz <105805284+jimmy110498@users.noreply.github.com>
2022-06-15 09:00:23 -0500

    Add: MappingConfiguration Implementation

--- src/main/java/com/turismo/backendturismotec/shared/mapping/MappingConfiguration.java
+++ src/main/java/com/turismo/backendturismotec/shared/mapping/MappingConfiguration.java
@@ -1,4 +1,12 @@
 package com.turismo.backendturismotec.shared.mapping;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("enhanceModelMapperConfiguration")
 public class MappingConfiguration {
+  @Bean
+    public EnhanceModelMapper modelMapper() {
+        return new EnhanceModelMapper();
+    }
 }

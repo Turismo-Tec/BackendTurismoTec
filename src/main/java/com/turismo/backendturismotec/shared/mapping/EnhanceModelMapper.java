6b10d4acc609acb489e7797440680b753dcb0d69
Jef de la cruz <105805284+jimmy110498@users.noreply.github.com>
2022-06-15 08:59:47 -0500

    Add: EnhanceModelMapper Implementation

--- src/main/java/com/turismo/backendturismotec/shared/mapping/EnhanceModelMapper.java
+++ src/main/java/com/turismo/backendturismotec/shared/mapping/EnhanceModelMapper.java
@@ -1,4 +1,18 @@
 package com.turismo.backendturismotec.shared.mapping;
 
-public class EnhanceModelMapper {
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EnhanceModelMapper extends ModelMapper{
+  
+     public EnhanceModelMapper(){
+        super();
+    }
+
+    public <S, T> List<T> mapList(List<S> sourceList, Class<T> targetClass){
+        return sourceList.stream().map(item -> this.map(item, targetClass)).collect(Collectors.toList());
+    }
+  
 }

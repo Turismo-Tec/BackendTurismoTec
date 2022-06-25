2cd43a5fa649436e476c2b7ac6b54e5af1a52380
RodrigoHuarcayaGalarza <89543373+RodrigoHuarcayaGalarza@users.noreply.github.com>
2022-06-25 13:38:20 -0500

    Add: Sale Service Implementation

---
+++ src/main/java/com/turismo/backendturismotec/turismo/services/SalesServiceImpl.java
@@ -0,0 +1,61 @@
+package com.turismo.backendturismotec.turismo.services;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class SalesServiceImpl implements SalesService {
+
+    private static final String ENTITY = "Sales";
+    private final SaleRepository saleRepository;
+    private final Validator validator;
+
+    public SalesServiceImpl(SaleRepository saleRepository, Validator validator) {
+        this.saleRepository = saleRepository;
+        this.validator = validator;
+    }
+
+    @Override
+    public List<Sales> getAll() {
+        return saleRepository.findAll();
+    }
+
+    @Override
+    public Sales getById(Long saleId) {
+        return saleRepository.findById(saleId)
+                .orElseThrow(() -> new ResourceNotFoundException(ENTITY, saleId));
+    }
+
+    @Override
+    public Sales create(Sales sales) {
+        Set<ConstraintViolation<Sales>> violations = validator.validate(sales);
+
+        if (!violations.isEmpty())
+            throw new ResourceValidationException(ENTITY, violations);
+
+        return saleRepository.save(sales);
+    }
+
+    @Override
+    public Sales update(Long id, Sales request) {
+        Set<ConstraintViolation<Sales>> violations = validator.validate(request);
+
+        if (!violations.isEmpty())
+            throw new ResourceValidationException(ENTITY, violations);
+
+
+        return saleRepository.findById(id).map(packages ->
+                        saleRepository.save(packages.withDate(request.getDate())))
+                .orElseThrow(() -> new ResourceNotFoundException(ENTITY, id));
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long saleId) {
+        return saleRepository.findById(saleId).map(packages -> {
+            saleRepository.delete(packages);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException(ENTITY, saleId));
+    }
+}

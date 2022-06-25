a077fd62fa756030f0c48f68e16722db7932d01b
RodrigoHuarcayaGalarza <89543373+RodrigoHuarcayaGalarza@users.noreply.github.com>
2022-06-25 09:16:51 -0500

    Add: Package Resource Implementation

--- src/main/java/com/turismo/backendturismotec/turismo/resources/packages/PackageResource.java
+++ src/main/java/com/turismo/backendturismotec/turismo/resources/packages/PackageResource.java
@@ -1,4 +1,18 @@
 package com.turismo.backendturismotec.turismo.resources.packages;
 
+@Setter
+@Getter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class PackageResource {
+  private String description;
+
+    private String country;
+
+    private float price;
+
+    //Relationships
+    //Client
+    //Companies
 }
